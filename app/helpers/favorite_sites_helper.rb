978e32a85fb0dfa54b6a94346deb53c95634412a
Yuri.Toyoda <protocolluna@gmail.com>
2015-09-22 15:42:06 +0000

    一覧の評価を画像に置き換え

--- app/helpers/favorite_sites_helper.rb
+++ app/helpers/favorite_sites_helper.rb
@@ -1,2 +1,19 @@
 module FavoriteSitesHelper
+  def rate_image(rate)
+    image_name = ""
+    case rate
+    when 1 then
+      image_name = "rate1.png"
+	when 2 then
+	  image_name = "rate2.png"
+	when 3 then
+	  image_name = "rate3.png"
+	when 4 then
+	  image_name = "rate4.png"
+	when 5 then
+	  image_name = "rate4.png"
+  	end
+
+  	image_name
+  end
 end
